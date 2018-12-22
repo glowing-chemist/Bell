fae0101a58d3ee8c345ba565d9403bc1bf526596
olie <olie.smit0@gmail.com>
2018-12-22 23:31:34 +0000

    Add some setters/getters to the image class.

--- Include/Core/Image.hpp
+++ Include/Core/Image.hpp
@@ -8,13 +8,14 @@
 #include "Core/GPUResource.hpp"
 #include "Core/DeviceChild.hpp"
 #include "Core/MemoryManager.hpp"
-
+#include "BarrierManager.hpp"
 
 class RenderDevice;
 
 
 class Image final : public GPUResource, public DeviceChild
 {
+	friend BarrierManager;
 public:
 
     Image(RenderDevice*,
@@ -38,6 +39,12 @@ public:
     uint32_t        numberOfMips() const { return mNumberOfMips; }
     void            generateMips(const uint32_t);
 
+	vk::Format		getFormat() const
+						{ return mFormat; }
+
+	vk::ImageLayout getLayout() const
+						{ return mLayout; }
+
     void            clear();
 
 private:
@@ -46,6 +53,7 @@ private:
     std::optional<vk::Sampler> mSampler; // only needed when the image is used as a texture
 
     vk::Format mFormat;
+	vk::ImageLayout mLayout;
     vk::ImageUsageFlags mUsage;
     uint32_t mNumberOfMips;
     vk::Extent3D mExtent;
