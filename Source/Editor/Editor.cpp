06373964d00e6bf6c58a5b1e7ea1ed1e2fb3f9e6
Oliver Smith <olie.smit0@gmail.com>
2019-05-07 20:52:42 +0100

    Add a WIP editor class. just to have this committed somewhere.

--- Source/Editor/Editor.cpp
+++ Source/Editor/Editor.cpp
@@ -1,2 +1,53 @@
 
+#include "Editor/Editor.h"
 
+#include "imgui.h"
+
+Editor::Editor(GLFWwindow* window) :
+    mWindow{window},
+    mCurrentCursorPos{0.0, 0.0},
+    mEngine{mWindow},
+    mInProgressScene{"In construction"}
+{
+    ImGui::CreateContext();
+}
+
+
+Editor::~Editor()
+{
+     ImGui::DestroyContext();
+}
+
+
+void Editor::run()
+{
+    while(!glfwWindowShouldClose(mWindow))
+    {
+        pumpInputQueue();
+    }
+}
+
+void Editor::renderScene()
+{
+
+}
+
+
+void Editor::renderOverlay()
+{
+
+}
+
+
+void Editor::swap()
+{
+
+}
+
+
+void Editor::pumpInputQueue()
+{
+    glfwPollEvents();
+
+    glfwGetCursorPos(mWindow, &mCurrentCursorPos.x, &mCurrentCursorPos.y);
+}
--- Source/Editor/Editor.h
+++ Source/Editor/Editor.h
@@ -1,4 +1,41 @@
 #ifndef EDITOR_HPP
 #define EDITOR_HPP
 
+#include "Engine/Engine.hpp"
+#include "Engine/Scene.h"
+
+#include "GLFW/glfw3.h"
+
+
+class Editor
+{
+public:
+    Editor(GLFWwindow*);
+    ~Editor();
+
+    void run();
+
+private:
+
+    void renderScene();
+    void renderOverlay();
+    void swap();
+
+    void pumpInputQueue();
+
+
+    GLFWwindow* mWindow;
+
+    struct CursorPosition
+    {
+        double x;
+        double y;
+    };
+    CursorPosition mCurrentCursorPos;
+
+    Engine mEngine;
+
+    Scene mInProgressScene;
+};
+
 #endif
