026af4b80e3828d70099ac9feaeb1127556fe869
Oliver Smith <olie.smit0@gmail.com>
2019-05-07 20:53:33 +0100

    Add a bare bones main for the editor executable.

--- Source/Editor/main.cpp
+++ Source/Editor/main.cpp
@@ -1,7 +1,17 @@
 
-
+#include "Editor/Editor.h"
 
 int main()
 {
 
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); // only resize explicitly
+    auto* window = glfwCreateWindow(1600, 900, "Bell Editor", nullptr, nullptr);
+
+    Editor editor{window};
+
+    editor.run();
+
+    glfwTerminate();
 }
