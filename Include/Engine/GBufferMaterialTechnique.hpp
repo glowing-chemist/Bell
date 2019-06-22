0330824cd71f57307a87d081730794eec29e17d3
Oliver Smith <olie.smit0@gmail.com>
2019-06-22 12:13:51 +0100

    Add the GBuffer material technique.

---
+++ Include/Engine/GBufferMaterialTechnique.hpp
@@ -0,0 +1,83 @@
+#ifndef GBUFFERMATERIAL_HPP
+#define GBUFFERMATERIAL_HPP
+
+#include "Technique.hpp"
+
+#include "Core/Image.hpp"
+#include "Core/ImageView.hpp"
+
+#include "RenderGraph/GraphicsTask.hpp"
+
+#include "Engine.hpp"
+
+
+class GBufferMaterialTechnique : public Technique<GraphicsTask>
+{
+public:
+	GBufferMaterialTechnique(Engine*);
+	virtual ~GBufferMaterialTechnique() = default;
+
+	virtual PassType getPassType() const final override
+		{ return PassType::GBufferMaterial; }
+
+	virtual GraphicsTask& getTaskToRecord() final override;
+
+	Image& getDepthImage()
+		{ return mDepthImage; }
+
+	ImageView& getDepthView()
+		{ return mDepthView; }
+
+	std::string getDepthName() const
+		{ return "GBuffer Depth"; }
+
+
+	Image& getMaterialImage()
+		{ return mMaterialImage; }
+
+	ImageView& getMaterialView()
+		{ return mMaterialView; }
+
+	std::string getAlbedoName() const
+		{ return "GBuffer Material"; }
+
+
+	Image& getNormalsImage()
+		{ return mNormalsImage; }
+
+	ImageView& getNormalsView()
+		{ return mNormalsView; }
+
+	std::string getNormalsName() const
+	 { return "GBuffer Normals"; }
+
+
+	Image& getUVImage()
+		{ return mUVImage; }
+
+	ImageView& getUVView()
+		{ return mUVView; }
+
+	std::string getUVName() const
+		{ return "GBuffer UV"; }
+
+private:
+
+	Image mDepthImage;
+	ImageView mDepthView;
+
+	Image mMaterialImage;
+	ImageView mMaterialView;
+
+	Image mNormalsImage;
+	ImageView mNormalsView;
+
+	Image mUVImage;
+	ImageView mUVView;
+
+	GraphicsPipelineDescription mPipelineDescription;
+	GraphicsTask mTask;
+	bool mTaskInitialised;
+};
+
+#endif
---
+++ Source/Engine/GBufferMaterialTechnique.cpp
@@ -0,0 +1,59 @@
+#include "Engine/GBufferMaterialTechnique.hpp"
+
+
+GBufferMaterialTechnique::GBufferMaterialTechnique(Engine* eng) :
+	Technique{"GBuffer", eng->getDevice()},
+
+	mDepthImage{getDevice(), Format::D24S8Float, ImageUsage::DepthStencil | ImageUsage::Sampled,
+				getDevice()->getSwapChain()->getSwapChainImageWidth(), getDevice()->getSwapChain()->getSwapChainImageHeight(),
+				1, 1, 1, 1, "Depth Buffer"},
+	mDepthView{mDepthImage, ImageViewType::Depth},
+
+	mMaterialImage{getDevice(), Format::R32Uint, ImageUsage::ColourAttachment | ImageUsage::Sampled,
+				 getDevice()->getSwapChain()->getSwapChainImageWidth(), getDevice()->getSwapChain()->getSwapChainImageHeight(),
+				 1, 1, 1, 1, "Material Buffer"},
+	mMaterialView{mMaterialImage, ImageViewType::Colour},
+
+	mNormalsImage{getDevice(), Format::R16G16Unorm, ImageUsage::ColourAttachment | ImageUsage::Sampled,
+				  getDevice()->getSwapChain()->getSwapChainImageWidth(), getDevice()->getSwapChain()->getSwapChainImageHeight(),
+				  1, 1, 1, 1, "Normals Buffer"},
+	mNormalsView{mNormalsImage, ImageViewType::Colour},
+
+	mUVImage{getDevice(), Format::RGBA32SFloat, ImageUsage::ColourAttachment | ImageUsage::Sampled,
+				   getDevice()->getSwapChain()->getSwapChainImageWidth(), getDevice()->getSwapChain()->getSwapChainImageHeight(),
+				   1, 1, 1, 1, "UV Buffer"},
+	mUVView{mUVImage, ImageViewType::Colour},
+
+	mPipelineDescription{eng->getShader("./Shaders/GBufferPassThroughMaterial.vert"),
+						 eng->getShader("./Shaders/GBufferMaterial.frag"),
+						 Rect{getDevice()->getSwapChain()->getSwapChainImageWidth(),
+							   getDevice()->getSwapChain()->getSwapChainImageHeight()},
+						 Rect{getDevice()->getSwapChain()->getSwapChainImageWidth(),
+						 getDevice()->getSwapChain()->getSwapChainImageHeight()}},
+
+	mTask{"GBufferMaterial", mPipelineDescription},
+	mTaskInitialised{false}
+{}
+
+
+GraphicsTask &GBufferMaterialTechnique::getTaskToRecord()
+{
+
+	if(!mTaskInitialised)
+	{
+		mTask.setVertexAttributes(VertexAttributes::Position4 | VertexAttributes::Material |
+								  VertexAttributes::Normals | VertexAttributes::TextureCoordinates);
+
+		mTask.addInput("CameraBuffer", AttachmentType::UniformBuffer);
+		mTask.addInput("ModelMatrix", AttachmentType::PushConstants);
+
+		mTask.addOutput("GBuffer Normals", AttachmentType::Texture2D, Format::R16G16Unorm, LoadOp::Clear_Black);
+		mTask.addOutput("GBuffer UV", AttachmentType::Texture2D, Format::RGBA8SRGB, LoadOp::Clear_Black);
+		mTask.addOutput("GBuffer Material", AttachmentType::Texture2D, Format::R8UNorm, LoadOp::Clear_Black);
+		mTask.addOutput("GBuffer Depth", AttachmentType::Depth, Format::D24S8Float, LoadOp::Clear_White);
+	}
+
+	mTask.clearCalls();
+
+	return mTask;
+}
