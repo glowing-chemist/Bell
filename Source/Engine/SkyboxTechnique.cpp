1cce7ef86585d354dab9cbf3a7650df258e4ede4
Oliver Smith <olie.smit0@gmail.com>
2019-11-04 17:40:54 +0000

    Add the depth attachment when rendering the skybox.

--- Source/Engine/SkyboxTechnique.cpp
+++ Source/Engine/SkyboxTechnique.cpp
@@ -11,13 +11,14 @@ SkyboxTechnique::SkyboxTechnique(Engine* eng) :
 							   getDevice()->getSwapChain()->getSwapChainImageHeight()},
 						 Rect{getDevice()->getSwapChain()->getSwapChainImageWidth(),
 						 getDevice()->getSwapChain()->getSwapChainImageHeight()},
-						 false, BlendMode::None, BlendMode::None, false, DepthTest::Equal, Primitive::LineList},
+                         false, BlendMode::None, BlendMode::None, false, DepthTest::Equal, Primitive::TriangleList},
 	mTask{"skybox", mPipelineDesc}
 {
 	mTask.addInput(kCameraBuffer, AttachmentType::UniformBuffer);
 	mTask.addInput(kSkyBox, AttachmentType::Texture2D);
 	mTask.addInput(kDefaultSampler, AttachmentType::Sampler);
-	mTask.addOutput(kFrameBufer, AttachmentType::RenderTarget2D, getDevice()->getSwapChainImage().getFormat());
+    mTask.addOutput(kFrameBufer, AttachmentType::RenderTarget2D, getDevice()->getSwapChainImage().getFormat());
+    mTask.addOutput(kGBufferDepth, AttachmentType::Depth, Format::D32Float);
 
 	mTask.addDrawCall(0, 3);
 }
