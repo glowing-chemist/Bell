6328006c877e5f6a340f4f4dab04950940e605da
oliver smith <olie.smit0@gmail.com>
2019-11-10 00:12:58 +0000

    Whitespace fix.

--- Include/Engine/AnalyticalImageBasedLightingTechnique.hpp
+++ Include/Engine/AnalyticalImageBasedLightingTechnique.hpp
@@ -14,13 +14,13 @@ public:
 
     virtual PassType getPassType() const override final
     {
-	return PassType::DeferredTextureAnalyticalPBRIBL;
+		return PassType::DeferredTextureAnalyticalPBRIBL;
     }
 
     // default empty implementations as most classes won't need to do anything for one of these.
     virtual void render(RenderGraph& graph, Engine*, const std::vector<const Scene::MeshInstance*>&) override final
     {
-	graph.addTask(mTask);
+		graph.addTask(mTask);
     }
 
     virtual void bindResources(RenderGraph&) const override final {}
