1d31933ac3539869d36e77f8457f551a74d4f839
Oliver Smith <olie.smit0@gmail.com>
2020-04-20 20:56:42 +0100

    Fix long standing shader include bug!.

--- Source/Core/Vulkan/VulkanShader.cpp
+++ Source/Core/Vulkan/VulkanShader.cpp
@@ -8,55 +8,49 @@
 
 namespace
 {
+    struct HeaderUserData
+    {
+        std::string path;
+        std::string source;
+    };
+
 	class ShaderIncluder : public shaderc::CompileOptions::IncluderInterface
 	{
 	public:
 
 		virtual shaderc_include_result* GetInclude(	const char* requested_source,
-													shaderc_include_type type,
-													const char* requesting_source,
-													size_t include_depth) override final
+                                                    shaderc_include_type,
+                                                    const char*,
+                                                    size_t) override final
 		{
 			const std::string headerPath = "./Shaders/" + std::string(requested_source);
-			auto it = mHeaderMap.find(headerPath);
-			if (it != mHeaderMap.end())
-			{
-				return &(it->second.result);
-			}
-			else
-			{
-				std::ifstream fileStream(headerPath);
-				std::string data{ (std::istreambuf_iterator<char>(fileStream)),
-					std::istreambuf_iterator<char>() };
 
-				BELL_ASSERT(data.c_str(), "Unable to read file");
+            std::ifstream fileStream(headerPath);
+            std::string data{ (std::istreambuf_iterator<char>(fileStream)),
+                std::istreambuf_iterator<char>() };
 
-				IncludeInfo entry{ shaderc_include_result{ headerPath.c_str(), headerPath.size(), data.c_str(), data.size(), nullptr }, std::move(data) };
-				auto newEntry = mHeaderMap.insert({ headerPath, std::move(entry) }).first;
+            BELL_ASSERT(data.c_str(), "Unable to read file");
 
-				return &newEntry->second.result;
-			}
+            HeaderUserData* userData = new HeaderUserData;
+            userData->path = std::move(headerPath);
+            userData->source = std::move(data);
+
+            shaderc_include_result* result = new shaderc_include_result;
+            result->source_name = userData->path.c_str();
+            result->source_name_length = userData->path.size();
+            result->content = userData->source.c_str();
+            result->content_length = userData->source.size();
+            result->user_data = userData;
+
+            return result;
 		}
 
-		virtual void ReleaseInclude(shaderc_include_result* data)
+        virtual void ReleaseInclude(shaderc_include_result* result) override final
 		{
-			// Don't need to do anything here, just free all resources at the end.
+            HeaderUserData* userData = static_cast<HeaderUserData*>(result->user_data);
+            delete userData;
+            delete result;
 		}
-
-		struct IncludeInfo
-		{
-			shaderc_include_result result;
-			std::string data;
-		};
-		const std::map<std::string, IncludeInfo>& getIncludedFiles() const
-		{
-			return mHeaderMap;
-		}
-
-	private:
-
-		std::map<std::string, IncludeInfo> mHeaderMap;
-
 	};
 
 }
@@ -80,22 +74,25 @@ bool VulkanShader::compile(const std::vector<std::string>& prefix)
 	shaderc::CompileOptions options;
 	options.SetSourceLanguage(shaderc_source_language::shaderc_source_language_hlsl);
 	options.SetOptimizationLevel(shaderc_optimization_level::shaderc_optimization_level_performance);
-	for (const auto define : prefix)
+    for (const auto& define : prefix)
 		options.AddMacroDefinition(define);
 	std::unique_ptr<shaderc::CompileOptions::IncluderInterface> includer = std::make_unique<ShaderIncluder>();
 	options.SetIncluder(std::move(includer));
-	options.SetTargetSpirv(shaderc_spirv_version::shaderc_spirv_version_1_3);
 	options.SetTargetEnvironment(shaderc_target_env::shaderc_target_env_vulkan, shaderc_env_version_vulkan_1_1);
 
 	shaderc::SpvCompilationResult result = compiler.CompileGlslToSpv(mSource.data(), mSource.size(), mShaderStage, mFilePath.string().c_str(), "main", options);
 
 	if (result.GetCompilationStatus() != shaderc_compilation_status::shaderc_compilation_status_success)
 	{
-		BELL_LOG("Shader compilation failed %s", result.GetErrorMessage().c_str())
+        auto preProcessResult = compiler.PreprocessGlsl(mSource.data(), mSource.size(), mShaderStage, mFilePath.string().c_str(), options);
+        std::string shaderSource(preProcessResult.begin(), preProcessResult.end());
+
+        BELL_LOG_ARGS("Shader compilation failed %s", result.GetErrorMessage().c_str())
+        BELL_LOG_ARGS("shader %s \nShader source \n%s", mFilePath.string().c_str() , shaderSource.c_str())
+        BELL_TRAP;
 
 		return false;
 	}
-
 	
 	mSPIRV.insert(mSPIRV.begin(), result.cbegin(), result.cend());
 	mCompiled = true;
@@ -110,7 +107,7 @@ bool VulkanShader::compile(const std::vector<std::string>& prefix)
 
     vk::ShaderModuleCreateInfo shaderModuleInfo{};
     shaderModuleInfo.setPCode(mSPIRV.data());
-    shaderModuleInfo.setCodeSize(mSPIRV.size() * sizeof(unsigned int));
+    shaderModuleInfo.setCodeSize(mSPIRV.size() * sizeof(uint32_t));
     mShaderModule = device->createShaderModule(shaderModuleInfo);
 
 	device->setDebugName(mFilePath.string(), *reinterpret_cast<uint64_t*>(&mShaderModule), VK_OBJECT_TYPE_SHADER_MODULE);
