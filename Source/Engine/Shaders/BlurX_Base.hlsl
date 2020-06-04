f311e3f7a140eaf7ccfe154808ff1429a2003678
Oliver Smith <olie.smit0@gmail.com>
2020-06-04 21:45:11 +0100

    Use loads here over samples to save a few tens of ms.

--- Source/Engine/Shaders/BlurX_Base.hlsl
+++ Source/Engine/Shaders/BlurX_Base.hlsl
@@ -2,30 +2,30 @@
 	uint xSize, ySize;
 	inImage.GetDimensions(xSize, ySize);
 
-	const float2 size = float2(xSize, ySize);
-
 	const int3 dispatchLocation = int3(globalIndex);
 
-	sharedLine[localIndex.x + 3] = inImage.Sample(linearSampler, float2(dispatchLocation.xy) / size);
+	if(dispatchLocation.x >= xSize)
+		return;
+
+	const float2 size = float2(xSize, ySize);
+
+	sharedLine[localIndex.x + 3] = inImage.Load(int3(dispatchLocation.xy, 0));
 
 	// Load the results either side as well.
 	if(localIndex.x < 3)
 	{
-		sharedLine[localIndex.x] = inImage.Sample(linearSampler, float2(dispatchLocation.xy - int2(3 - localIndex.x, 0)) / size);
+		sharedLine[localIndex.x] = inImage.Load(int3(dispatchLocation.xy - int2(3 - localIndex.x, 0), 0));
 	}
 
 	if(localIndex.x == (KERNEL_SIZE - 1))
 	{
-		sharedLine[KERNEL_SIZE + 3] = inImage.Sample(linearSampler, float2(dispatchLocation.xy + int2(1,0)) / size);
-		sharedLine[KERNEL_SIZE + 4] = inImage.Sample(linearSampler, float2(dispatchLocation.xy + int2(2,0)) / size);
-		sharedLine[KERNEL_SIZE + 5] = inImage.Sample(linearSampler, float2(dispatchLocation.xy + int2(3,0)) / size);
+		sharedLine[KERNEL_SIZE + 3] = inImage.Load(int3(dispatchLocation.xy + int2(1,0), 0));
+		sharedLine[KERNEL_SIZE + 4] = inImage.Load(int3(dispatchLocation.xy + int2(2,0), 0));
+		sharedLine[KERNEL_SIZE + 5] = inImage.Load(int3(dispatchLocation.xy + int2(3,0), 0));
 	}
 
 	// wait for the shared memory to be fully populated.
 	AllMemoryBarrierWithGroupSync();
-
-	if(dispatchLocation.x >= size.x)
-		return;
 
 	outImage[dispatchLocation.xy] = blur(localIndex.x);
 }
--- Source/Engine/Shaders/BlurY_Base.hlsl
+++ Source/Engine/Shaders/BlurY_Base.hlsl
@@ -2,30 +2,28 @@
 	uint xSize, ySize;
 	inImage.GetDimensions(xSize, ySize);
 
-	const float2 size = float2(xSize, ySize);
-
 	const int3 dispatchLocation = int3(globalIndex);
 
-	sharedLine[localIndex.y + 3] = inImage.Sample(linearSampler, float2(dispatchLocation.xy) / size);
+	if(dispatchLocation.y >= ySize)
+		return;
+
+	sharedLine[localIndex.y + 3] = inImage.Load(int3(dispatchLocation.xy, 0));
 
 	// Load the results either side as well.
 	if(localIndex.y < 3)
 	{
-		sharedLine[localIndex.y] = inImage.Sample(linearSampler, float2(dispatchLocation.xy - int2(0, 3 - localIndex.y)) / size);
+		sharedLine[localIndex.y] = inImage.Load(int3(dispatchLocation.xy - int2(0, 3 - localIndex.y), 0));
 	}
 
 	if(localIndex.y == (KERNEL_SIZE - 1))
 	{
-		sharedLine[KERNEL_SIZE + 3] = inImage.Sample(linearSampler, float2(dispatchLocation.xy + int2(0,1)) / size);
-		sharedLine[KERNEL_SIZE + 4] = inImage.Sample(linearSampler, float2(dispatchLocation.xy + int2(0,2)) / size);
-		sharedLine[KERNEL_SIZE + 5] = inImage.Sample(linearSampler, float2(dispatchLocation.xy + int2(0,3)) / size);
+		sharedLine[KERNEL_SIZE + 3] = inImage.Load(int3(dispatchLocation.xy + int2(0,1), 0));
+		sharedLine[KERNEL_SIZE + 4] = inImage.Load(int3(dispatchLocation.xy + int2(0,2), 0));
+		sharedLine[KERNEL_SIZE + 5] = inImage.Load(int3(dispatchLocation.xy + int2(0,3), 0));
 	}
 
 	// wait for the shared memory to be fully populated.
 	AllMemoryBarrierWithGroupSync();
-
-	if(dispatchLocation.y >= size.y)
-		return;
 
 	outImage[dispatchLocation.xy] = blur(localIndex.y);
 }
