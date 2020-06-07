9f972f8b86af8bca36c33d4fe79871ede9bf22db
oliver smith <olie.smit0@gmail.com>
2020-06-07 11:17:32 +0100

    Specify the blur type for compute blur shaders.

--- Source/Engine/Shaders/GaussianBlur.hlsl
+++ Source/Engine/Shaders/GaussianBlur.hlsl
@@ -1,14 +1,14 @@
 #define KERNEL_SIZE 256
 
 // extra for eaxtr samples required at end.
-groupshared float4 sharedLine[KERNEL_SIZE + (2 * 3)];
+groupshared VEC_TYPE sharedLine[KERNEL_SIZE + (2 * 3)];
 
 
-float4 blur(uint position)
+VEC_TYPE blur(uint position)
 {
 	const float decomposedKernel[] =  {0.030078323, 0.104983664, 0.222250419, 0.285375187, 0.222250419, 0.104983664, 0.030078323};
 
-	float4 bluredPixel = float4(0.0f, 0.0f, 0.0f, 0.0f);
+	VEC_TYPE bluredPixel = VEC_TYPE(0.0f);
 
 	[unroll]
 	for(uint i = 0; i < 7; ++i)
