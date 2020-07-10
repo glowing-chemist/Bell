a88a71280e8a7453f8741b5c11976db877d581d2
Oliver Smith <olie.smit0@gmail.com>
2020-07-10 16:48:10 +0100

    Add WIP light probe files.

---
+++ Source/Engine/Shaders/LightProbes.hlsl
@@ -0,0 +1,11 @@
+
+struct SphericalHarmonic
+{
+	float3 position;
+	float coefs[27];
+};
+
+float3 calculateIrradiance(const float3 positionWS, const float3 normal, const float3 probeStart, const float3 probeSize, const float3 probeDensity)
+{
+
+}
