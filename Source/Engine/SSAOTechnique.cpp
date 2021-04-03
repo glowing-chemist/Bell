f96f5e1a240c0f418fbba2a77adb81a369b6dda6
oliver smith <olie.smit0@gmail.com>
2021-04-03 18:13:09 +0100

    Unlock SSAO sample count.

--- Source/Engine/SSAOTechnique.cpp
+++ Source/Engine/SSAOTechnique.cpp
@@ -17,19 +17,7 @@ namespace
 {
     float2 generateSphericalOffsets(const uint32_t n)
     {
-        static float2 offsets[16]{
-                float2(0.5381, 0.1856), float2(0.1379, 0.2486),
-                float2(0.3371, 0.5679), float2(-0.6999, -0.0451),
-                float2(0.0689, -0.1598), float2(0.0560, 0.0069),
-                float2(-0.0146, 0.1402), float2(0.0100, -0.1924),
-                float2(-0.3577, -0.5301), float2(-0.3169, 0.1063),
-                float2(0.0103, -0.5869), float2(-0.0897, -0.4940),
-                float2(0.7119, -0.0154), float2(-0.0533, 0.0596),
-                float2(0.0352, -0.0631), float2(-0.4776, 0.2847) };
-
-        return offsets[n];
-
-        //return float2(((rand() / float(RAND_MAX)) - 0.5f) * 2.0f, ((rand() / float(RAND_MAX)) - 0.5f) * 2.0f);
+        return float2(((rand() / float(RAND_MAX)) - 0.5f) * 2.0f, ((rand() / float(RAND_MAX)) - 0.5f) * 2.0f);
     }
 }
 
